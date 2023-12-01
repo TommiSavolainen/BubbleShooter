419fcb2fb550ad317b84a05916f92e6135f5a218
Tommi <tommi.savolainen79@gmail.com>
2023-12-01 10:16:27 +0200

    luotu pallo luokka

--- main.js
+++ main.js
@@ -7,12 +7,17 @@ let player_x = canvas.width / 2;
 let player_y = canvas.height - 30;
 let ammus_x = canvas.width / 2;
 let ammus_y = canvas.height - 30;
-
+let palloRadius = 10;
 let playerRadius = 10;
 let playerColor = 'pink';
 let colors = ['red', 'green', 'yellow', 'blue', 'pink'];
 let players = [];
 let ammukset = [];
+let pallot = [];
+let rivi = 6;
+let paikka = 1;
+let pallo_x = 11;
+let pallo_y = 11;
 class Player {
     constructor(x, y, radius, color, velocity) {
         this.x = x;
@@ -35,8 +40,34 @@ class Player {
         this.y = this.y + this.velocity.y;
     }
 }
+class Pallo {
+    constructor(x, y, radius, color, rivi, paikka, merkattu) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.rivi = rivi;
+        this.paikka = paikka;
+        this.merkattu = merkattu;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.stroke();
+    }
+    update() {
+        this.draw();
+    }
+}
+for (i = 0; i < 36; i++) {
+    pallot.push(new Pallo(pallo_x, pallo_y, palloRadius, colors[Math.floor(Math.random() * colors.length)], rivi, paikka, (merkattu = false)));
+    pallo_x += 21;
+    paikka += 1;
+}
 for (i = 0; i < 1; i++) {
-    // player_x -= 30;
     players.push(new Player(player_x, player_y, playerRadius, colors[Math.floor(Math.random() * colors.length)], { x: 1, y: 1 }));
 }
 
@@ -48,6 +79,9 @@ function animate() {
     });
     players.forEach((player) => {
         player.draw();
+    });
+    pallot.forEach((pallo) => {
+        pallo.draw();
     });
 }
 addEventListener('click', (e) => {
